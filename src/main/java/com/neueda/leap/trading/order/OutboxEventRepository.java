8bcaeb70053c928f1aea9fb42185e8c97177ceae
John Page <john@thejohnpage.com>
2026-09-17 15:56:12 +0000

    feat: query unpublished outbox events

--- src/main/java/com/neueda/leap/trading/order/OutboxEventRepository.java
+++ src/main/java/com/neueda/leap/trading/order/OutboxEventRepository.java
@@ -1,7 +1,5 @@
 package com.neueda.leap.trading.order;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OutboxEventRepository extends JpaRepository<OutboxEvent, UUID> {
+import java.util.List; import java.util.UUID; import org.springframework.data.jpa.repository.JpaRepository;
+public interface OutboxEventRepository extends JpaRepository<OutboxEvent,UUID>{
+ List<OutboxEvent> findTop50ByPublishedAtIsNullOrderByCreatedAtAsc();
 }
