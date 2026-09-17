bf59298d248e14e707e84120d4c98bf58a5a8b70
John Page <john@thejohnpage.com>
2026-09-17 15:56:32 +0000

    feat: enable transactional outbox scheduling

--- src/main/java/com/neueda/leap/trading/TradingApplication.java
+++ src/main/java/com/neueda/leap/trading/TradingApplication.java
@@ -1,12 +1,4 @@
 package com.neueda.leap.trading;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TradingApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TradingApplication.class, args);
-    }
-}
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.scheduling.annotation.EnableScheduling;
+@SpringBootApplication @EnableScheduling
+public class TradingApplication { public static void main(String[] args){SpringApplication.run(TradingApplication.class,args);} }
