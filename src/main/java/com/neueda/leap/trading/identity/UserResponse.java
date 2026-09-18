41b1db2f795fdd2dfcab170a7c053ed883e5c41b
John Page <john@thejohnpage.com>
2026-09-18 17:56:55 +0000

    feat: add administrative user response

---
+++ src/main/java/com/neueda/leap/trading/identity/UserResponse.java
@@ -0,0 +1,16 @@
+package com.neueda.leap.trading.identity;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public record UserResponse(
+        UUID userId,
+        String email,
+        String firstName,
+        String lastName,
+        boolean active,
+        UUID clientId,
+        List<String> roles,
+        Instant createdAt,
+        Instant updatedAt) {}
