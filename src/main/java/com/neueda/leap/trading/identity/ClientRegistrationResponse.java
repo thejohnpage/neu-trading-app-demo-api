c2b2cbe0e44752bf564ed156d29f7a76d6be4bb2
John Page <john@thejohnpage.com>
2026-09-18 18:15:53 +0000

    refactor: remove admin user identity from client registration response

--- src/main/java/com/neueda/leap/trading/identity/ClientRegistrationResponse.java
+++ src/main/java/com/neueda/leap/trading/identity/ClientRegistrationResponse.java
@@ -2,9 +2,4 @@ package com.neueda.leap.trading.identity;
 
 import java.util.UUID;
 
-public record ClientRegistrationResponse(
-        UUID clientId,
-        UUID userId,
-        UUID accountId,
-        String accountNumber,
-        String email) {}
+public record ClientRegistrationResponse(UUID clientId,UUID accountId,String accountNumber,String email) {}
