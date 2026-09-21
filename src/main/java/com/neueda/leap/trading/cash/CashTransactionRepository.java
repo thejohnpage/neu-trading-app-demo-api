4334852830cfb6f7e31ba01855298e11b0f78d48
John Page <john@thejohnpage.com>
2026-09-21 14:48:44 -0500

    Add cash transaction history query

--- src/main/java/com/neueda/leap/trading/cash/CashTransactionRepository.java
+++ src/main/java/com/neueda/leap/trading/cash/CashTransactionRepository.java
@@ -1,3 +1,9 @@
 package com.neueda.leap.trading.cash;
-import java.util.UUID; import org.springframework.data.jpa.repository.JpaRepository;
-public interface CashTransactionRepository extends JpaRepository<CashTransaction,UUID> {}
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CashTransactionRepository extends JpaRepository<CashTransaction,UUID> {
+ List<CashTransaction> findByAccountIdOrderByCreatedAtDesc(UUID accountId);
+}
