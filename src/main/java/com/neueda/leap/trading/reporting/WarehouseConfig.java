2ff0c51d9f2b3f3fd350fdb7e607a0546fbfe6c2
John Page <john@thejohnpage.com>
2026-09-23 16:38:54 -0500

    Finalize warehouse MyBatis session factory

--- src/main/java/com/neueda/leap/trading/reporting/WarehouseConfig.java
+++ src/main/java/com/neueda/leap/trading/reporting/WarehouseConfig.java
@@ -5,5 +5,5 @@ import javax.sql.DataSource; import org.apache.ibatis.session.SqlSessionFactory;
 @MapperScan(basePackages="com.neueda.leap.trading.reporting",sqlSessionFactoryRef="warehouseSqlSessionFactory")
 public class WarehouseConfig {
  @Bean(name="warehouseDataSource",defaultCandidate=false) DataSource warehouseDataSource(@Value("${app.warehouse.url:jdbc:postgresql://localhost:55433/trading_dw}")String url,@Value("${app.warehouse.username:trading_dw}")String username,@Value("${app.warehouse.password:trading_dw_change_me}")String password){return DataSourceBuilder.create().url(url).username(username).password(password).driverClassName("org.postgresql.Driver").build();}
- @Bean(name="warehouseSqlSessionFactory") SqlSessionFactory warehouseSqlSessionFactory(@Qualifier("warehouseDataSource")DataSource ds)throws Exception{SqlSessionFactoryBean bean=new SqlSessionFactoryBean();bean.setDataSource(ds);bean.getObject();return bean.getObject();}
+ @Bean(name="warehouseSqlSessionFactory") SqlSessionFactory warehouseSqlSessionFactory(@Qualifier("warehouseDataSource")DataSource ds)throws Exception{SqlSessionFactoryBean bean=new SqlSessionFactoryBean();bean.setDataSource(ds);return bean.getObject();}
 }
