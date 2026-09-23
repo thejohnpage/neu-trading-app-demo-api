75ba6c0af66c3fa7baf777de433a032da639f8ec
John Page <john@thejohnpage.com>
2026-09-23 15:57:33 -0500

    Read API version dynamically from POM build info

--- src/main/java/com/neueda/leap/trading/api/SystemController.java
+++ src/main/java/com/neueda/leap/trading/api/SystemController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name="System",description="Application metadata and health-related information")
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1")
 public class SystemController {
- private final String applicationName; private final String applicationVersion;
- public SystemController(@Value("${spring.application.name}") String applicationName,@Value("${info.app.version:unknown}") String applicationVersion){this.applicationName=applicationName;this.applicationVersion=applicationVersion;}
+ private final String applicationName; private final BuildProperties buildProperties;
+ public SystemController(@Value("${spring.application.name}") String applicationName,BuildProperties buildProperties){this.applicationName=applicationName;this.buildProperties=buildProperties;}
  /** Get API version. */
  @Operation(summary="Get API version",description="Returns the application name, configured version and current server timestamp.")
  @GetMapping("/version")
- public Map<String,Object> version(){return Map.of("application",applicationName,"version",applicationVersion,"timestamp",Instant.now().toString());}
+ public Map<String,Object> version(){return Map.of("application",applicationName,"version",buildProperties.getVersion(),"timestamp",Instant.now().toString());}
 }
